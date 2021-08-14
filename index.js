64451a2207f3f5a4f337a15f41f114722461849e
Lokesh Gurung <lokeshgurungwj@gmail.com>
2021-08-14 12:41:32 +0545

    Add the Tailwind CSS frontend

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => {
+    console.log(`The server is listening at port ${port}.`);
+});
+
